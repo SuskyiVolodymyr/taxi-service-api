fd8d4845b35835cfdb1eab1a9f2748d1df1f72bf
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 15:07:50 +0300

    add city model

--- taxi/models.py
+++ taxi/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
 
-class Car(models.Model):
-    model = models.CharField(max_length=255)
-    number = models.CharField(max_length=255)
-    driver = None
-    price_per_meter = models.DecimalField(max_digits=5, decimal_places=2)
+class City(models.Model):
+    name = models.CharField(max_length=255)
