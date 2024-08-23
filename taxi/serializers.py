c836e15a60b53b6d5ee6221931d4ed6605ca7db6
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 16:56:00 +0300

    add driver application feature, actions for approve or reject and fire existing driver

--- taxi/serializers.py
+++ taxi/serializers.py
@@ -1,9 +1,52 @@
 from rest_framework import serializers
 
-from taxi.models import City
+from taxi.models import City, DriverApplication, Driver
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ("id", "name")
+
+
+class DriverApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DriverApplication
+        fields = (
+            "id",
+            "license_number",
+            "age",
+            "city",
+            "sex",
+            "user",
+            "status",
+        )
+        read_only_fields = ("id", "user")
+
+    def validate(self, attrs):
+        if attrs["age"] < 18:
+            raise serializers.ValidationError(
+                "Driver must be at least 18 years old"
+            )
+        if self.context["request"].user.is_driver:
+            raise serializers.ValidationError("User is already a driver")
+        existing_application = DriverApplication.objects.filter(
+            user=self.context["request"].user, status="P"
+        )
+        if existing_application:
+            raise serializers.ValidationError(
+                "User already applied for a driver"
+            )
+        return attrs
+
+    def create(self, validated_data):
+        driver_application = DriverApplication.objects.create(
+            user=self.context["request"].user, **validated_data
+        )
+        return driver_application
+
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = ("id", "license_number", "age", "city", "sex", "rate", "user")
--- taxi/views.py
+++ taxi/views.py
@@ -1,11 +1,99 @@
-from rest_framework.viewsets import ModelViewSet
+from django.db import transaction
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins, status
 
-from taxi.models import City
+from taxi.models import City, DriverApplication, Driver
 from taxi.permissions import IsAdminOrReadOnly
-from taxi.serializers import CitySerializer
+from taxi.serializers import (
+    CitySerializer,
+    DriverApplicationSerializer,
+    DriverSerializer,
+)
 
 
 class CityViewSet(ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
     permission_classes = [IsAdminOrReadOnly]
+
+
+class DriverApplicationViewSet(ModelViewSet):
+    queryset = DriverApplication.objects.all()
+    serializer_class = DriverApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.queryset.all()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+    @action(
+        detail=True,
+        methods=["get"],
+        permission_classes=[IsAdminUser],
+    )
+    def apply(self, request, pk: int = None):
+        with transaction.atomic():
+            application = self.get_object()
+            if application.status != "P":
+                return Response(
+                    "Application already processed",
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            user = application.user
+            driver = Driver.objects.create(
+                user=user,
+                license_number=application.license_number,
+                age=application.age,
+                city=application.city,
+                sex=application.sex,
+            )
+            user.is_driver = True
+            application.status = "A"
+            application.save()
+            driver_serializer = DriverSerializer(driver)
+            return Response(
+                driver_serializer.data, status=status.HTTP_201_CREATED
+            )
+
+    @action(
+        detail=True,
+        methods=["get"],
+        permission_classes=[IsAdminUser],
+    )
+    def reject(self, request, pk: int = None):
+        with transaction.atomic():
+            application = self.get_object()
+            if application.status != "P":
+                return Response(
+                    "Application already processed",
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            application.status = "R"
+            application.save()
+            serializer = DriverApplicationSerializer(application)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class DriverViewSet(
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet
+):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+    @action(
+        detail=True,
+        methods=["get"],
+        permission_classes=[IsAdminUser],
+    )
+    def fire(self, request, pk: int = None):
+        with transaction.atomic():
+            driver = self.get_object()
+            user = driver.user
+            user.is_driver = False
+            driver.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
