e1a8778e6fb811ee39fa16d95ebb22b1b4438635
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 15:12:51 +0300

    add car model

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+from user.models import Driver
+
 
 class City(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    number = models.CharField(max_length=255)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    price_per_meter = models.DecimalField(max_digits=5, decimal_places=2)
