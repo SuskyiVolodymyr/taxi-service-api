9e5be9bb21f575c89f2daf8da4707079d0a36b55
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 15:05:11 +0300

    add AUTH_USER_MODEL

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "user.User"
+
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
