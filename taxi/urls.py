00e3a17d2ec2e750b466470da6317064a982d313
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 16:56:18 +0300

    add driver application and drivers urls

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,10 +1,17 @@
+from django.urls import path
 from rest_framework import routers
 
-from taxi.views import CityViewSet
+from taxi.views import CityViewSet, DriverApplicationViewSet, DriverViewSet
 
 app_name = "taxi"
 
 router = routers.DefaultRouter()
 router.register("cities", CityViewSet)
+router.register("driver_applications", DriverApplicationViewSet)
+router.register("drivers", DriverViewSet)
+
+# urlpatterns = [
+#     path("driver_applications/", DriverApplicationViewSet.as_view(), name="driver_applications"),
+# ]
 
 urlpatterns = router.urls
