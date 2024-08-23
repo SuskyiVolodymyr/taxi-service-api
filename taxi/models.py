8748d3b03bcbba504685128679fc7f854eb80b0f
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-23 23:49:48 +0300

    add order model

--- taxi/models.py
+++ taxi/models.py
@@ -55,3 +55,25 @@ class DriverApplication(models.Model):
 
     def __str__(self):
         return self.user.full_name
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_CHOICES = {
+        ("1", "Pending"),
+        ("2", "Paid"),
+        ("3", "Cancelled"),
+    }
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    street_from = models.CharField(max_length=255)
+    street_to = models.CharField(max_length=255)
+    distance = models.IntegerField()
+    payment_status = models.CharField(
+        max_length=6, choices=PAYMENT_STATUS_CHOICES, default="1"
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.date_created}"
