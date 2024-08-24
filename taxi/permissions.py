ea28da933ab9fb97ac4aa5735e446f1ccc99e72d
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-24 14:41:25 +0300

    fixed IsDriverOrAdminUser permission

--- taxi/permissions.py
+++ taxi/permissions.py
@@ -11,6 +11,6 @@ class IsAdminOrReadOnly(BasePermission):
 
 class IsDriverOrAdminUser(BasePermission):
     def has_permission(self, request: Request, *args, **kwargs) -> bool:
-        return request.user and (
+        return request.user.is_authenticated and (
             request.user.is_driver or request.user.is_staff
         )
