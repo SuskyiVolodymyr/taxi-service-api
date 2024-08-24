21d5746e59654b83d441e1efb7d3902e81433d72
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-24 13:16:07 +0300

    add payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django_enum import EnumField
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusEnum(models.TextChoices):
+        pending = "1", "Pending"
+        paid = "2", "Paid"
+        canceled = "3", "Canceled"
+
+    status = EnumField(StatusEnum)
+    session_url = models.URLField(max_length=500)
+    session_id = models.CharField(max_length=100)
+    money_to_pay = models.DecimalField(decimal_places=2, max_digits=10)
