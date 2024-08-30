e3b97638276703768ed2091d28aa8b868fe60ba0
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-30 21:32:25 +0300

    fix dry problem in test_car_api.py

--- taxi/tests/base.py
+++ taxi/tests/base.py
@@ -1,17 +1,26 @@
+from django.conf.global_settings import AUTH_USER_MODEL
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from taxi.models import Driver, Car, City
+
 
 class TestBase(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
+        self.default_city = City.objects.create(name="test city")
+        self.default_user = get_user_model().objects.create_user(
             email="test@test.com",
             first_name="test",
             last_name="test",
             password="test1234",
         )
-        self.admin = get_user_model().objects.create_superuser(
+        self.default_driver_user = self.sample_user(
+            email="driver_user@test.com", is_driver=True
+        )
+        self.default_driver = self.sample_driver(self.default_driver_user)
+        self.default_car = self.sample_car(self.default_driver)
+        self.default_admin = get_user_model().objects.create_superuser(
             email="admin@admin.com",
             first_name="admin",
             last_name="admin",
@@ -19,3 +28,35 @@ class TestBase(TestCase):
         )
 
         self.client = APIClient()
+
+    def sample_car(self, driver: Driver, **params) -> Car:
+        payload = {
+            "model": "test model",
+            "number": "test number",
+            "driver": driver,
+        }
+        payload.update(**params)
+        return Car.objects.create(**payload)
+
+    def sample_driver(self, user: AUTH_USER_MODEL, **params) -> Driver:
+        payload = {
+            "user": user,
+            "license_number": "123456",
+            "age": 18,
+            "city": self.default_city,
+            "sex": "M",
+        }
+        payload.update(**params)
+        return Driver.objects.create(**payload)
+
+    @staticmethod
+    def sample_user(email: str, **params) -> AUTH_USER_MODEL:
+        payload = {
+            "email": email,
+            "first_name": "test",
+            "last_name": "test",
+            "password": "test1234",
+            "is_driver": False,
+        }
+        payload.update(**params)
+        return get_user_model().objects.create_user(**payload)
--- taxi/tests/test_car_api.py
+++ taxi/tests/test_car_api.py
@@ -2,10 +2,8 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-from taxi.models import Car, Driver, City
 from taxi.serializers import CarSerializer
 from taxi.tests.base import TestBase
-from taxi_service.settings import AUTH_USER_MODEL
 
 CAR_URL = reverse("taxi:car-list")
 
@@ -15,27 +13,13 @@ def get_car_detail(car_id) -> str:
 
 
 class UnauthorizedCarAPITest(TestBase):
+    def setUp(self):
+        super().setUp()
+
     def test_unauthorized_user_cant_list_cars(self):
         res = self.client.get(CAR_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    @staticmethod
-    def sample_car(driver: Driver):
-        return Car.objects.create(
-            model="test model",
-            number="test number",
-            driver=driver,
-        )
-
-    def sample_driver(self):
-        return Driver.objects.create(
-            user=self.user,
-            license_number="123456",
-            age=18,
-            city=City.objects.create(name="test city"),
-            sex="M",
-        )
 
     def test_unauthorized_user_cant_create_cars(self):
         payload = {
@@ -47,22 +31,16 @@ class UnauthorizedCarAPITest(TestBase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_unauthorized_user_cant_update_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
         payload = {
             "model": "test model",
             "number": "test number",
         }
-        res = self.client.patch(get_car_detail(car.id), payload)
+        res = self.client.patch(get_car_detail(self.default_car.id), payload)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_unauthorized_user_cant_delete_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
-        res = self.client.delete(get_car_detail(car.id))
+        res = self.client.delete(get_car_detail(self.default_car.id))
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -70,35 +48,12 @@ class UnauthorizedCarAPITest(TestBase):
 class SimpleUserCarAPITest(TestBase):
     def setUp(self):
         super().setUp()
-        self.client.force_authenticate(user=self.user)
-        self.user2 = get_user_model().objects.create_user(
-            email="test2@test.com",
-            first_name="test2",
-            last_name="test2",
-            password="test1234",
-        )
+        self.client.force_authenticate(user=self.default_user)
 
     def test_simple_user_cant_list_cars(self):
         res = self.client.get(CAR_URL)
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
-
-    @staticmethod
-    def sample_car(driver: Driver):
-        return Car.objects.create(
-            model="test model",
-            number="test number",
-            driver=driver,
-        )
-
-    def sample_driver(self):
-        return Driver.objects.create(
-            user=self.user2,
-            license_number="123456",
-            age=18,
-            city=City.objects.create(name="test city"),
-            sex="M",
-        )
 
     def test_simple_user_cant_create_cars(self):
         payload = {
@@ -110,22 +65,16 @@ class SimpleUserCarAPITest(TestBase):
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_simple_user_cant_update_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
         payload = {
             "model": "test model",
             "number": "test number",
         }
-        res = self.client.patch(get_car_detail(car.id), payload)
+        res = self.client.patch(get_car_detail(self.default_car.id), payload)
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_simple_user_cant_delete_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
-        res = self.client.delete(get_car_detail(car.id))
+        res = self.client.delete(get_car_detail(self.default_car.id))
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -133,37 +82,9 @@ class SimpleUserCarAPITest(TestBase):
 class DriverCarAPITest(TestBase):
     def setUp(self):
         super().setUp()
-        self.user.is_driver = True
-        self.user.save()
-        self.client.force_authenticate(user=self.user)
-        self.user2 = get_user_model().objects.create_user(
-            email="test2@test.com",
-            first_name="test2",
-            last_name="test2",
-            password="test1234",
-        )
-
-    @staticmethod
-    def sample_car(driver: Driver):
-        return Car.objects.create(
-            model="test model",
-            number="test number",
-            driver=driver,
-        )
-
-    def sample_driver(self, user: AUTH_USER_MODEL = None):
-        if user is None:
-            user = self.user
-        return Driver.objects.create(
-            user=user,
-            license_number="123456",
-            age=18,
-            city=City.objects.create(name="test city"),
-            sex="M",
-        )
+        self.client.force_authenticate(user=self.default_driver_user)
 
     def test_driver_can_create_cars(self):
-        self.sample_driver()
         payload = {
             "model": "test model",
             "number": "test number",
@@ -178,12 +99,13 @@ class DriverCarAPITest(TestBase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_driver_can_list_only_his_cars(self):
-        driver1 = self.sample_driver()
-        driver2 = self.sample_driver(user=self.user2)
-        car1 = self.sample_car(driver1)
+        driver_user_2 = self.sample_user(
+            email="driver_user_2@test.com", is_driver=True
+        )
+        driver2 = self.sample_driver(user=driver_user_2)
         car2 = self.sample_car(driver2)
 
-        serializer1 = CarSerializer(car1)
+        serializer1 = CarSerializer(self.default_car)
         serializer2 = CarSerializer(car2)
 
         res = self.client.get(CAR_URL)
@@ -192,22 +114,16 @@ class DriverCarAPITest(TestBase):
         self.assertNotIn(serializer2.data, res.data)
 
     def test_driver_can_update_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
         payload = {
-            "model": "test model",
-            "number": "test number",
+            "model": "updated test model",
+            "number": "updated test number",
         }
-        res = self.client.patch(get_car_detail(car.id), payload)
+        res = self.client.patch(get_car_detail(self.default_car.id), payload)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_driver_can_delete_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
-        res = self.client.delete(get_car_detail(car.id))
+        res = self.client.delete(get_car_detail(self.default_car.id))
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
 
@@ -215,78 +131,40 @@ class DriverCarAPITest(TestBase):
 class AdminCarAPITest(TestBase):
     def setUp(self):
         super().setUp()
-        self.client.force_authenticate(user=self.admin)
-
-    @staticmethod
-    def sample_car(driver: Driver):
-        return Car.objects.create(
-            model="test model",
-            number="test number",
-            driver=driver,
-        )
-
-    def sample_driver(self, user: AUTH_USER_MODEL = None) -> Driver:
-        if user is None:
-            user = self.user
-        return Driver.objects.create(
-            user=user,
-            license_number="123456",
-            age=18,
-            city=City.objects.create(name="test city"),
-            sex="M",
-        )
-
-    def test_admin_can_list_cars(self):
-        res = self.client.get(CAR_URL)
-
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.client.force_authenticate(user=self.default_admin)
 
     def test_admin_can_list_all_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
-        serializer = CarSerializer(car)
+        serializer = CarSerializer(self.default_car)
 
         res = self.client.get(CAR_URL)
 
         self.assertIn(serializer.data, res.data)
 
     def test_car_list_with_driver_filter(self):
-        self.user2 = get_user_model().objects.create_user(
-            email="test2@test.com",
-            first_name="test2",
-            last_name="test2",
-            password="test1234",
+        driver_user_2 = self.sample_user(
+            email="driver_user_2@test.com", is_driver=True
         )
-        driver = self.sample_driver()
-        driver2 = self.sample_driver(user=self.user2)
-        car1 = self.sample_car(driver)
+        driver2 = self.sample_driver(user=driver_user_2)
         car2 = self.sample_car(driver2)
 
-        serializer1 = CarSerializer(car1)
+        serializer1 = CarSerializer(self.default_car)
         serializer2 = CarSerializer(car2)
 
-        res = self.client.get(CAR_URL, {"driver": driver.id})
+        res = self.client.get(CAR_URL, {"driver": self.default_driver.id})
 
         self.assertIn(serializer1.data, res.data)
         self.assertNotIn(serializer2.data, res.data)
 
     def test_admin_can_update_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
         payload = {
             "model": "test model",
             "number": "test number",
         }
-        res = self.client.patch(get_car_detail(car.id), payload)
+        res = self.client.patch(get_car_detail(self.default_car.id), payload)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_admin_can_delete_cars(self):
-        driver = self.sample_driver()
-        car = self.sample_car(driver)
-
-        res = self.client.delete(get_car_detail(car.id))
+        res = self.client.delete(get_car_detail(self.default_car.id))
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
